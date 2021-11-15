8e1572016346a56a3b27629a7785ae010d7b80ca
Teramoto Takeru <1911140013m@ed.fuk.kindai.ac.jp>
2021-11-15 07:12:30 +0000

    コメントview仮を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,19 @@ class CommentsController < ApplicationController
   
   def create
     user = User.find_by(uid: session[:login_uid])
-    #@comment = Comment.new(, user_id: user.id,)
+    @comment = Comment.new(user_comment: params[:word][:comment],
+                                user_id: user.id,
+                                word_id: word_id)
+    if @comment.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to root_path
   end
 end
