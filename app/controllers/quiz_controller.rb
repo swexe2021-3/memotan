0cd18b5f7e56c12bdcbd05f76d38a626d1aa1311
Teramoto Takeru <1911140013m@ed.fuk.kindai.ac.jp>
2021-12-14 09:33:07 +0000

    クイズのシャッフルを追加

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -3,6 +3,7 @@ class QuizController < ApplicationController
     def start
         word = Word.where(purpose_id: params[:purpose_id])
         @@quizlist = word.pluck(:id)
+        @@quizlist.shuffle!
         @quizlist_w_id = @@quizlist.pop
         render 'next_quiz'
     end
